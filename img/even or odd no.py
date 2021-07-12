f980162223af3e81051fda33adfadddaf723b9cc
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2021-07-12 18:00:55 +0530

    resources

---
+++ img/even or odd no.py
@@ -0,0 +1,7 @@
+#ask user to an integer you check whether even or odd
+number=int(input("enter a number: "))
+
+if(number%2==0):
+    print("it is an even number")
+else:
+    print("number is odd ")
---
+++ img/marks.py
@@ -0,0 +1,12 @@
+marks=99
+if (marks>=90):
+    print("you got a new phone")
+elif(marks>=75 and marks<90):
+    print("extra 100 hours on screen time")
+elif (marks>=50 and marks<75):
+    print("no screen for a week")
+    print("inside 50-75 band")
+else:
+    print("inside less than 50-75 band")
+    print("too bad to playing, no screen, no phone, study study....")
+print("out of if")
